25dddb30365578172bcbb389563926019902d73b
Patrick Walkowicz <patrykwalkowicz@gmail.com>
2015-09-17 20:21:26 +0100

    Added revertString problem

---
+++ revertString/revertString.js
@@ -0,0 +1,8 @@
+function revertString(input) {
+	// Your code
+}
+
+// Some test cases
+
+//console.log(reverseString("RaNdom")); //should return MoDnar
+//console.log("JavaScript"); //should return "JpirCsavaj"
