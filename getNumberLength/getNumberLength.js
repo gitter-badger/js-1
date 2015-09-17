264b7742d3cc1ec360f6bf4072530488ae2c712b
mattia spinelli <mattia@macbooks-Macintosh.local>
2015-09-17 14:05:13 +0200

    get NumberLength corrected

--- getNumberLength/getNumberLength.js
+++ getNumberLength/getNumberLength.js
@@ -2,35 +2,37 @@
 
 if( isNaN(n)){ console.log("wrong input"); return "wrong input"} 
 
-else if (n.toString().length < 22) {console.log("wrong input"); return "wrong input"} 
+ 
 
 else { 
-
+    console.log(n.toString())
 	rawStr = n.toString();
 	splitted = rawStr.split("");
-	//console.log(splitted);
+	console.log(splitted);
 	mark = 0;
 	base = "";
 
 	multiplier = "";
 	 
 	//aquire index of exponential values
-	for(i = 0; i< splitted.length; i++) { if(splitted[i] == "+"){mark = i }
+	for(i = 0; i< splitted.length; i++) { if(splitted[i] == "+"){mark = i; console.log("mark =" + mark)}
 	}
-
+    if (mark == 0) {console.log("wrong input"); return "wrong input"}
+   
 	//acquire number of zeroes
 	for (i = mark; i<mark+(splitted.length-mark-1); i++){multiplier += splitted[i+1]};
-	//console.log("multiplier = " + multiplier)
+	console.log("multiplier = " + multiplier)
     
     result = parseInt(multiplier)+1;
 
-	console.log("Number length is " + result);
-
-    return result;
+	console.log("Number length is " + result)
   
 }
 }
-getNumberLength(123456789012345678901230)
+getNumberLength(9000000000000000000000000000000000)
+
+getNumberLength(1234567890123456789012)
+
 
 
 
