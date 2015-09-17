b841fd6848627596fa0745fd858dad699270b821
mattia spinelli <mattia@macbooks-Macintosh.local>
2015-09-17 13:47:28 +0200

    get NumberLength working for me

--- getNumberLength/getNumberLength.js
+++ getNumberLength/getNumberLength.js
@@ -1,11 +1,36 @@
  function getNumberLength(n){
-//your code here
-} 
-  var error = "wrong input";
-  return (typeof n !== "number" || n.toString().length < 21) ? "wrong input" : n.toString().length;
-}
 
-// Wrote some test cases as well
-console.log(getNumberLength(""));
-console.log(getNumberLength(948435689037698457643586347345789603));
-console.log(getNumberLength(94843568903));
+if( isNaN(n)){ console.log("wrong input"); return "wrong input"} 
+
+else if (n.toString().length < 22) {console.log("wrong input"); return "wrong input"} 
+
+else { 
+
+	rawStr = n.toString();
+	splitted = rawStr.split("");
+	//console.log(splitted);
+	mark = 0;
+	base = "";
+
+	multiplier = "";
+	 
+	//aquire index of exponential values
+	for(i = 0; i< splitted.length; i++) { if(splitted[i] == "+"){mark = i }
+	}
+
+	//acquire number of zeroes
+	for (i = mark; i<mark+(splitted.length-mark-1); i++){multiplier += splitted[i+1]};
+	//console.log("multiplier = " + multiplier)
+    
+    result = parseInt(multiplier)+1;
+
+	console.log("Number length is " + result);
+
+    return result;
+  
+}
+}
+getNumberLength(123456789012345678901230)
+
+
+
