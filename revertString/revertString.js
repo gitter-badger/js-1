3a9eae2fe6e3457cdb5ae42fd814f6396b39e7a0
spudtrader <jmurphy.web@gmail.com>
2015-09-22 11:10:01 +0700

    Jack's Updated revertString function

--- revertString/revertString.js
+++ revertString/revertString.js
@@ -1,34 +1,11 @@
 function revertString(input) {
-	// Your code
-
-var length= input.length;
-negative = new Array();
-	for(i=0; i< length; i++){
-		if(input[i] == input[i].toUpperCase()){negative.push(true)}
-		else {negative.push(false)}
-	}
-console.log(negative)
-
-midput = input.toLowerCase()
-console.log(midput)
-
-midput2 = reverse(midput)
-
-output = midput2.split("")
-
-//make uppercase in output
-for(i=0; i< length; i++){
-	if(negative[i]){ output[i] = output[i].toUpperCase()}
+  var reversed = "";
+  var array = input.split("").reverse();
+  for(var i = 0; i < input.length; i++){
+    if(input[i] === input[i].toUpperCase()){ 
+      reversed += array[i].toUpperCase();
+    }
+    else { reversed += array[i].toLowerCase(); }
+  }
+  return reversed;
 }
-
-console.log(output)
-}
-function reverse(s){
-    return s.split("").reverse().join("");
-}
-
-
-// Some test cases
-
-revertString("RaNdom"); //should return MoDnar
-//console.log("JavaScript"); //should return "JpirCsavaj"
