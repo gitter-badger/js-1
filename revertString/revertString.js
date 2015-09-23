fabe8675cab1eaa4b6f713bfd7d029877ed52976
spudtrader <jmurphy.web@gmail.com>
2015-09-23 07:11:07 +0700

    update revertstring

--- revertString/revertString.js
+++ revertString/revertString.js
@@ -1,11 +1,21 @@
 function revertString(input) {
   var reversed = "";
-  var array = input.split("").reverse();
-  for(var i = 0; i < input.length; i++){
-    if(input[i] === input[i].toUpperCase()){ 
-      reversed += array[i].toUpperCase();
-    }
-    else { reversed += array[i].toLowerCase(); }
+  var array1 = input.split(" ");
+  var array2 = [];
+  for (var i = 0; i < array1.length; i++) {
+  	array2[i] = array1[i].split("").reverse();
   }
+for(var j = 0; j <= array1.length-1; j++){
+  for(var k = 0; k < array2[j].length; k++){
+    console.log(j,k);
+    if(array1[j][k] === array1[j][k].toUpperCase()){ 
+      reversed += array2[j][k].toUpperCase();
+    }
+    else { reversed += array2[j][k].toLowerCase(); }
+  }
+  if(j !== array1.length-1){
+    reversed += " ";
+  }
+}
   return reversed;
 }
