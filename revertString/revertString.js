2b9a089fc1b6ad022425d562b0de8fba12df1871
thevalent <thevalentz@gmail.com>
2015-09-24 22:51:22 +0100

    Update revertString.js

--- revertString/revertString.js
+++ revertString/revertString.js
@@ -1,40 +1,30 @@
-function revertMoreStrings(words) {
-  
-var strArray = words.split(" ");
-var counter=0;
-  
-for(var s of strArray){
-     strArray[counter]=revertString(s);
-     counter++;
-}
-return strArray.join(" ");
-  
+function revertMoreStrings( words ) {
+  var strArray = words.split(" "); // -> ["Hello", "World"]
+  var counter = 0;
+    	for( var s of strArray ){
+        	strArray[counter] = revertString(s); // -> ["Olleh"] ; ["Olleh","Dlrow"]
+        	counter++;
+    	}
+  return strArray.join(" "); // -> "Olleh Dlrow"
 }  
 
-function revertString(input) {
-
-var length= input.length;
-var negative = [];
-  for(i=0; i< length; i++){
-    if(input[i] == input[i].toUpperCase()){negative.push(true);}
-    else {negative.push(false);}
-  }
-
-var output = reverse(input.toLowerCase()).split("");
-
-//make uppercase in output
-
-for(i=0; i< length; i++){
-  if(negative[i]){ output[i] = output[i].toUpperCase();}
+function revertString( input ) {
+  var length = input.length;
+  var negative = [];
+    	for( i = 0; i < length; i++ ){
+    		  if( input[i] == input[i].toUpperCase() ) { negative.push(true); }
+    		  else { negative.push(false); }
+    	}
+  var output = reverse( input.toLowerCase() ).split("");
+    	for( i = 0 ; i < length; i++ ){
+    		  if( negative[i] ){ output[i] = output[i].toUpperCase(); }
+    	}
+  return ( output.join("") );
 }
 
-return (output.join(""));
+function reverse( s ) {
+  return s.split("").reverse().join("");
 }
 
-function reverse(s){
-    return s.split("").reverse().join("");
-}
-
-
-// Some test cases
-console.log(revertMoreStrings("Hello World"));// output : "Olleh Dlrow"
+// Test case
+console.log( revertMoreStrings( "Hello World" ) ); // output -> "Olleh Dlrow"
