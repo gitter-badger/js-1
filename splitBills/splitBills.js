253bc33fc022ce0b1cb0a6a65b473101efeec014
franzmoro <franzmoro@hotmail.com>
2015-11-11 17:44:40 +0100

    updated splitBills.js end of file and added 'var' declaration for new variables.

--- splitBills/splitBills.js
+++ splitBills/splitBills.js
@@ -1,13 +1,11 @@
 function splitTheBill(x) {
-  sum=0;
-  num=0;
+  var sum = 0;
+  var num = 0;
   for(var k in x){
     if(x.hasOwnProperty(k)){ sum += x[k]; num++ }
   };
-  avg=sum/num; 
-  owes=new Object();
+  var avg=sum/num; 
+  var owes=new Object();
   for(k in x){ owes[k]=Math.round((x[k]-avg+ 0.00001) * 100) / 100 };
   return owes;
 }
-
-console.log(splitTheBill({A: 40, B: 25, X: 10}))
