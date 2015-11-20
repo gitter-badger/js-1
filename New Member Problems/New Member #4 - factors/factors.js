40bf4c2157c7d4440bd32ea3d33a7a4bb2460a46
Nalin_C <nalin.chhibber@gmail.com>
2015-11-20 18:42:36 +0530

    Update array name

--- New Member Problems/New Member #4 - factors/factors.js
+++ New Member Problems/New Member #4 - factors/factors.js
@@ -1,12 +1,12 @@
 function factors(number){
 	if(isNaN(number) || Math.sign(number)==-1)
 		return -1;
-	var factors = [],
+	var factorsArray = [],
 	quotient = 0;
 
 	for(var i = number; i > 0 ; i--,quotient = number/i)
 		if(quotient === Math.floor(quotient))
-			factors.push(i); 
+			factorsArray.push(i); 
 
-	return factors;
+	return factorsArray;
 }
