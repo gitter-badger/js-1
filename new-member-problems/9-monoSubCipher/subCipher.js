b3efe34049ab1abf436db8aa8c5b3e0fd75a43f1
thevjm <thevjm@gmail.com>
2015-12-15 22:49:25 +0000

    syntax errors

--- new-member-problems/9-monoSubCipher/subCipher.js
+++ new-member-problems/9-monoSubCipher/subCipher.js
@@ -1,8 +1,10 @@
 function subCipher(text) {
   return text.toUpperCase().replace(/([A-Z])/g, function(x) {
-    if (x == 'Z')
+    if (x == 'Z') {
       return 'A';
-    else
-      return String.fromCharCode(x.charCodeAt(0) + 1)
+    }
+    else {
+      return String.fromCharCode(x.charCodeAt(0) + 1);
+    }
   });
 };
