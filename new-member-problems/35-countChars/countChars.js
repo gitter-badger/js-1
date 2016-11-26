44f68875438c3b5c6e31d1114db4c19522bd5cea
Maja Kudlicka <maja.kudlicka@gmail.com>
2016-11-26 14:32:33 +0000

    Add files via upload

--- new-member-problems/35-countChars/countChars.js
+++ new-member-problems/35-countChars/countChars.js
@@ -1,3 +1,25 @@
+
+function char_count(str,char){
+  count = 0;
+  for(var i=0;i<str.length;i++){
+    if(str.charAt(i)==char){
+      count++;
+    }
+  }
+  
+  return count;
+}
+
+
 function countChars(arrayOfStrings, char) {
-  // oops - function empty - an issue to be reported
+
+  occurences = [];
+for(var i=0;i<arrayOfStrings.length;i++){  
+var obj = { elementIndex: i,   
+            counter: char_count(arrayOfStrings[i],char),   
+           };
+           occurences.push(obj);
+}
+
+
 }
